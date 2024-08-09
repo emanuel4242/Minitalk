a410157ca912f14e6cbe26a6c598fd662d884e14
Emanuel Malungo <151544343+emanuel4242@users.noreply.github.com>
2024-08-09 14:40:12 +0000

    Escrevaendo o header

--- include/minitalk.h
+++ include/minitalk.h
@@ -10,4 +10,25 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef MINITAILK_H
+# define MINITALK_H
 
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <signal.h>
+# include <string.h>
+
+// function for client
+
+void	error_exit(unsigned int server_pid, char *str);
+void	send_char(unsigned int server_pid, const char *str);
+void    send_signal(unsigned int server_pid, unsigned char c);
+void	send_signal(unsigned int server_pid, unsigned char c);
+
+int	ft_isdigit(int i);
+int	ft_atoi(const char *nptr);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putstr_fd(char *s, int fd);
+
+#endif
